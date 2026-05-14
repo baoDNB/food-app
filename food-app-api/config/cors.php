9830dabe55d2c8a88e1d49c041253f4e8b2ba6c0
Bao <baopa0805@gmail.com>
2026-05-14 12:09:02 +0700

    ok 14

--- food-app-api/config/cors.php
+++ food-app-api/config/cors.php
@@ -19,10 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://food-app-phi-black.vercel.app', // Link trang web của bạn trên Vercel
-        'http://localhost:3000',                // Để bạn vẫn test được dưới máy
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
